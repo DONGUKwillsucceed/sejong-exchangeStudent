fbdb84f229157444c6fcdb2448034a3c37711c19
jeli01 <jeli01@naver.com>
2022-05-14 16:04:43 +0900

    auth function added

--- src/routes/community.js
+++ src/routes/community.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import * as communityController from '../controller/community.js'
+import { isAuth } from '../middleware/auth.js';
 const route = express.Router();
 
-route.get('/:country/:id', communityController.getAdata);
-route.get('/:country', communityController.getList);
-route.post('/:country', communityController.postData);     //id 뺄게요
-route.put('/:country/:id', communityController.putData);   // country 필요없긴 함
-route.delete('/:country/:id', communityController.removeData); // 얘도 country 필요없긴 함
+route.get('/:country/:id', isAuth, communityController.getAdata);
+route.get('/:country', isAuth, communityController.getList);
+route.post('/:country', isAuth, communityController.postData);     //id 뺄게요
+route.put('/:country/:id', isAuth, communityController.putData);   // country 필요없긴 함
+route.delete('/:country/:id', isAuth, communityController.removeData); // 얘도 country 필요없긴 함
 
 export default route;
--- src/routes/document.js
+++ src/routes/document.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import * as documentController from '../controller/document.js'
+import { isAuth } from '../middleware/auth.js';
 const route = express.Router();
 
-route.get('/:country/:school/:id', documentController.getAdata);  // 해당 게시물 출력
-route.get('/:country/:school', documentController.getPostingList);      // 해당 학교 게시물들 출력
-route.get('/:country', documentController.getSchoolList);            // 여러 학교들 출력
-route.post('/:country/:school', documentController.postData);         // 게시물 create
-route.put('/:country/:school/:id', documentController.putData);       // 게시물 update
-route.delete('/:country/:school/:id', documentController.removeData);  // 게시물 delete
+route.get('/:country/:school/:id', isAuth, documentController.getAdata);  // 해당 게시물 출력
+route.get('/:country/:school', isAuth, documentController.getPostingList);      // 해당 학교 게시물들 출력
+route.get('/:country', isAuth, documentController.getSchoolList);            // 여러 학교들 출력
+route.post('/:country/:school', isAuth, documentController.postData);         // 게시물 create
+route.put('/:country/:school/:id', isAuth, documentController.putData);       // 게시물 update
+route.delete('/:country/:school/:id', isAuth, documentController.removeData);  // 게시물 delete
 
 export default route;
