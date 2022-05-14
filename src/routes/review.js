c6b9810bf9580d77b43a7fe319b827d370a30913
jeli01 <jeli01@naver.com>
2022-05-14 16:00:24 +0900

    Revert "auth function add"
    This reverts commit f3812a6383e29d6b212d8be6fc33cbd80ba31040.

--- src/routes/review.js
+++ src/routes/review.js
@@ -1,12 +1,11 @@
 import express from 'express';
 import * as reviewController from '../controller/review.js'
-import { isAuth } from '../middleware/auth.js';
 const route = express.Router();
 
-route.get('/:country/:id', isAuth, reviewController.getAdata);
-route.get('/:country', isAuth, reviewController.getList);
-route.post('/:country', isAuth, reviewController.postData);     //id 뺄게요
-route.put('/:country/:id', isAuth, reviewController.putData);   // country 필요없긴 함
-route.delete('/:country/:id', isAuth, reviewController.removeData); // 얘도 country 필요없긴 함
+route.get('/:country/:id', reviewController.getAdata);
+route.get('/:country', reviewController.getList);
+route.post('/:country', reviewController.postData);     //id 뺄게요
+route.put('/:country/:id', reviewController.putData);   // country 필요없긴 함
+route.delete('/:country/:id', reviewController.removeData); // 얘도 country 필요없긴 함
 
 export default route;
